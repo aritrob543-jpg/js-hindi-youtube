04dee525f77286832c75e45fe18649f6f8c9b984
aritrob543-jpg <aritrob543@gmail.com>
2025-10-04 19:44:17 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId=14453
+let accountEmail="aritrob543@gmail.com"
+var accountPassword="12345"
+accountCity="jaipur"
+
+accountCity="bengaluru"
+accountEmail="oireo"
+
+
+//account id=2 not allowed bcz const cannot be changed
+//let,var,account city can be changed
+// prefer not to use var because issue in scope
+
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use treat";//treat all js code as newer version
+//alert(3+3) we are using node js,not browser
+let name="Hitesh"
+let age=3
+let isloggedin=false;
+
+
+//number=>2 to the power 53
+//bigint
+//string
+//boolean
+//null
+//undefined
+//symbol=>unique
+//object
+console.log(typeof("Aritro"));
+console.log(typeof(undefined));//undefined
+
+console.log(typeof(null)); //object
+
