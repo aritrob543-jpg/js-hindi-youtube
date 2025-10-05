599003ce2514ed3f20fc620987d1dd68d6362804
aritrob543-jpg <aritrob543@gmail.com>
2025-10-05 13:14:22 +0000

    comparison of js

--- 01_basics/conversion.js
+++ 01_basics/conversion.js
@@ -5,3 +5,9 @@ console.log(valueInNumber)//NaN=not a number
 console.log(typeof valueInNumber)
 //""=false
 //"name"=true
+//operations
+console.log(2**2)//2 to the power 2
+console.log(2**3)//2 to the power 3
+console.log(1+"2"+"2")//122 if string at first then eveything treated as string
+console.log(1+3+"2")//42
+console.log(+true)//1
