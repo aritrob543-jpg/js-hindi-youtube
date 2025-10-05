f38a35605bbaf75a4da2fc7d32cbdb4ba539982d
aritrob543-jpg <aritrob543@gmail.com>
2025-10-05 12:53:53 +0000

    conversionin datatypes

---
+++ 01_basics/conversion.js
@@ -0,0 +1,7 @@
+let score="33abc"
+console.log(typeof score)
+let valueInNumber=Number(score)
+console.log(valueInNumber)//NaN=not a number
+console.log(typeof valueInNumber)
+//""=false
+//"name"=true
