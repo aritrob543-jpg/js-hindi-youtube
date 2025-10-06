2c7b37348a313933aae8da381d34f3b1427f1a0f
aritrob543-jpg <aritrob543@gmail.com>
2025-10-06 19:45:34 +0000

    array

--- 01_basics/02_basics/01_array.js
+++
@@ -1,3 +0,0 @@
-const myarr=[1,2,3,4,5]
-myarr.push(6)
-console.log(myarr)
---
+++ 02_basics/01_array.js
@@ -0,0 +1,8 @@
+const myarr=[1,2,3,4,5,6]
+const myn1=myarr.slice(0,2)
+console.log("A",myarr);
+console.log(myn1);
+const myn2=myarr.splice(0,2)
+console.log("b",myarr)
+console.log(myn2);
+
