4354f158ca1cb4e1031b571f6a5c61af2a53c2a5
aritrob543-jpg <aritrob543@gmail.com>
2025-10-06 19:12:38 +0000

    array related things

---
+++ 01_basics/02_basics/01_array.js
@@ -0,0 +1,3 @@
+const myarr=[1,2,3,4,5]
+myarr.push(6)
+console.log(myarr)
---
+++ 01_basics/date.js
@@ -0,0 +1,8 @@
+let mydate=new Date();//object
+console.log(mydate.toDateString());
+let mycreateddate=new Date(2025,8,9)
+console.log(mycreateddate.toDateString());//month start from 0
+let myTimeStamp=Date.now()
+console.log(myTimeStamp)
+
+
